73478c4bd34d18843782569bdf9ab92e29267a26
Evan Moses <emoses@gmail.com>
2014-06-05 22:28:05 -0700

    Adding setup for vim packages

--- install.sh
+++ install.sh
@@ -6,3 +6,16 @@ do
     ln -s $DIR/$file $HOME/${file#dot}
 done
 ln -s $DIR/gitignore $HOME/gitignore
+echo "Setting up vim..."
+mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle $HOME/.vim/colors
+if [ ! -e $HOME/.vim/autoload/pathogen.vim ] 
+then
+    curl 'www.vim.org/scripts/download_script.php?src_id=21650' > /tmp/pathogen.zip
+    unzip /tmp/pathogen.zip -d $HOME/.vim/
+    rm /tmp/pathogen.zip
+fi
+if [ ! -e $HOME/.vim/colors/spectro.vim ]
+then
+    curl 'http://www.vim.org/scripts/download_script.php?src_id=5356' > $HOME/.vim/colors/spectro.vim
+fi
+
