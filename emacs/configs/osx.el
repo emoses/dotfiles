5fb4b1067052ddcda51e19fe00ce49d409422815
Evan Moses <emoses@salesforce.com>
2015-02-08 13:45:19 -0800

    Making ediff behave on osx when window is fullscreen

--- emacs/configs/osx.el
+++ emacs/configs/osx.el
@@ -2,3 +2,17 @@
 (setq mac-option-modifier 'alt)
 (setq mac-control-modifier 'meta)
 (add-to-list 'exec-path "/usr/local/bin")
+
+(defun my:frame-fullscreen-p ()
+  "Is the current frame fullscreen?.  See toggle-frame-fullscreen in frames.el"
+  (memq (frame-parameter nil 'fullscreen) '(fullscreen fullboth)))
+
+;;If the emacs window is in fullscreen on osx, the ediff control frame will
+;;show up on another desktop.  This will fix that by opening it in the same
+;;frame if we're currently maximized 
+(setq ediff-window-setup-function
+      (lambda (&rest args)
+          (if (my:frame-fullscreen-p)
+              (apply #'ediff-setup-windows-plain args)
+            (apply #'ediff-setup-windows-default args))))
+                                       
