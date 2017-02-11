fc58f8876c2dacea1fad558e51a90ab48493a6f2
Evan Moses <evan@emoses.org>
2017-02-10 16:42:29 -0800

    Update clojure and i3 configs

--- emacs/configs/evil.el
+++ emacs/configs/evil.el
@@ -9,10 +9,11 @@
   ;;Mode which start in emacs state
   (add-to-list 'evil-emacs-state-modes 'dired-mode)
   (add-to-list 'evil-emacs-state-modes 'cider-docview-mode)
+  (add-to-list 'evil-emacs-state-modes 'git-rebase-mode)
 
   ;;Start in insert state for repl
   (add-to-list 'evil-insert-state-modes 'cider-repl-mode)
-  (add-to-list 'evil-emacs-state-modes 'git-rebase-mode)
+  (add-to-list 'evil-insert-state-modes 'inf-clojure-mode)
 
   (add-hook 'emacs-lisp-mode-hook
             (lambda ()
