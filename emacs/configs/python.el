9fbd8ed889b07e4b6234e45a8f2f275135b1ccd9
Evan Moses <evan@emoses.org>
2019-02-04 11:04:42 -0800

    Switch python to use-package; cleans up some :config problems

--- emacs/configs/python.el
+++ emacs/configs/python.el
@@ -1,71 +1,72 @@
 (use-package pyenv-mode)
 
-
-
-(eval-after-load 'python-mode
-  (progn
+(use-package python
+  :mode ("\\.py\\'" . python-mode)
+  :after pyenv-mode
+  :bind (:map pyenv-mode-map
+              ("C-c C-s" . nil)
+              :map python-mode-map
+              ("C-c C-o" . #'my:python-occur-definitions)
+              ("C-c C-s" . #'my:projectile-ag-symbol)
+              ("C-c C-a" . #'python-lineify-arguments))
+  :config
                                         ; extracted from elpy
-    (pyenv-mode)
-    (setq python-shell-interpreter "python3")
-    (defun my:python-occur-definitions ()
-      "Display an occur buffer of all definitions in the current buffer.
+  (require 'subr-x)
+  (pyenv-mode)
+  (setq python-shell-interpreter "python3")
+  (defun my:python-occur-definitions ()
+    "Display an occur buffer of all definitions in the current buffer.
 Also, switch to that buffer."
-      (interactive)
-      (let ((list-matching-lines-face nil))
-        (occur "^\s*\\(\\(async\s\\|\\)def\\|class\\)\s"))
-      (let ((window (get-buffer-window "*Occur*")))
-        if window
-        (select-window window)
-
-        (switch-to-buffer "*Occur*")))
-    (defun my:projectile-ag-symbol (search-term &optional arg)
-      "Run an ag search for symbol at point, or region if active.
+    (interactive)
+    (let ((list-matching-lines-face nil))
+      (occur "^\s*\\(\\(async\s\\|\\)def\\|class\\)\s"))
+    (when-let ((window (get-buffer-window "*Occur*")))
+      (select-window window))
+    (switch-to-buffer "*Occur*"))
+  (defun my:projectile-ag-symbol (search-term &optional arg)
+    "Run an ag search for symbol at point, or region if active.
 
 With optional prefix ARG, SEARCH-TERM is treated as a regexp"
-      (interactive
-       (list
-        (let ((symbol
-               (if (use-region-p)
-                   (buffer-substring-no-properties (region-beginning)
-                                                   (region-end))
-                 (thing-at-point 'symbol))))
-          (if (and symbol (not current-prefix-arg))
-              symbol
-            (projectile--read-search-string-with-default
-             (format "Search in project for %s: " (if current-prefix-arg "regexp" "string")))))
-        current-prefix-arg))
-      (projectile-ag search-term arg))
+    (interactive
+     (list
+      (let ((symbol
+             (if (use-region-p)
+                 (buffer-substring-no-properties (region-beginning)
+                                                 (region-end))
+               (thing-at-point 'symbol))))
+        (if (and symbol (not current-prefix-arg))
+            symbol
+          (projectile--read-search-string-with-default
+           (format "Search in project for %s: " (if current-prefix-arg "regexp" "string")))))
+      current-prefix-arg))
+    (projectile-ag search-term arg))
 
-    (defun python-lineify-arguments ()
-      "TODO: make idempotent, handle more errors"
-      (interactive)
-      (when (python-syntax-comment-or-string-p)
-        (error "Cannot use this function inside a string or comment"))
-      (save-excursion
-        (python-nav-up-list -1)
-        (while (not (equal (char-after) ?\)))
-          (forward-char)
-          (if (not (eolp))
+  (defun python-lineify-arguments ()
+    "TODO: make idempotent, handle more errors"
+    (interactive)
+    (when (python-syntax-comment-or-string-p)
+      (error "Cannot use this function inside a string or comment"))
+    (save-excursion
+      (python-nav-up-list -1)
+      (while (not (equal (char-after) ?\)))
+        (forward-char)
+        (if (not (eolp))
             (newline-and-indent)
-            (forward-line))
-          (condition-case nil
+          (forward-line))
+        (condition-case nil
             (while (not (equal (char-after) ?,))
               (python-nav-forward-sexp))
-            (error
-             (search-forward ")" nil nil)
-             (backward-char))))
-        (when (not (equal (char-before) ?,))
-          (insert ?,)
-          (newline-and-indent))))
+          (error
+           (search-forward ")" nil nil)
+           (backward-char))))
+      (when (not (equal (char-before) ?,))
+        (insert ?,)
+        (newline-and-indent))))
 
-    (define-key pyenv-mode-map (kbd "C-c C-s") nil)
-    (define-key python-mode-map (kbd "C-c C-o") #'my:python-occur-definitions)
-    (define-key python-mode-map (kbd "C-c C-s") #'my:projectile-ag-symbol)
-    (define-key python-mode-map (kbd "C-c C-a") #'python-lineify-arguments)
-    (unbind-key (kbd "C-c C-p") python-mode-map) ;Unbind run-python, which is easy to mis-hit with projectile
-    (set-variable 'python-indent-def-block-scale 1)
-    ;; (add-hook 'python-mode-hook 'flycheck-mode)
-    ))
+  (unbind-key (kbd "C-c C-p") python-mode-map) ;Unbind run-python, which is easy to mis-hit with projectile
+  (set-variable 'python-indent-def-block-scale 1)
+  ;; (add-hook 'python-mode-hook 'flycheck-mode)
+  )
 
 ;; (use-package elpy
 ;;   :after flycheck
@@ -85,7 +86,7 @@ With optional prefix ARG, SEARCH-TERM is treated as a regexp"
 
 (use-package lsp-mode
   :bind (("C-c M-r" . lsp-rename))
-  :after (python-mode)
+  :after (python)
   :init
   (add-hook 'python-mode-hook 'lsp)
   ;; lsp-python-enable is created by macro above
@@ -94,10 +95,6 @@ With optional prefix ARG, SEARCH-TERM is treated as a regexp"
   ;; change nil to 't to enable logging of packets between emacs and the LS
   ;; this was invaluable for debugging communication with the MS Python Language Server
   ;; and comparing this with what vs.code is doing
-
-
-
-
   (setq lsp-print-io nil)
   (defun my:lsp--filter-variables (filter-fn sym)
     (if (= 13 (gethash "kind" sym))
