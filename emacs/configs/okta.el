28531707bbf884df101e929a50bfeabb962b6587
Evan Moses <evan@emoses.org>
2023-12-28 07:33:26 -0800

    Adding git-credential-helper auth-source

--- emacs/configs/okta.el
+++ emacs/configs/okta.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 (when (boundp 'okta)
   (require 'gotest)
 
@@ -12,9 +13,10 @@
     (save-some-buffers (not compilation-ask-about-save))
     (compile command))
 
-  (defun -okta-integration-test (args)
+  (defun -okta-integration-test (args &optional unit-only)
     (save-some-buffers (not compilation-ask-about-save))
-    (go-test--go-test args "SFT_DB_AUTOCLEAN=true SFT_DB_INTEGRATION_TESTS=true"))
+    (let ((env (format  "SFT_DB_AUTOCLEAN=true SFT_DB_INTEGRATION_TESTS=%s" (if unit-only "false" "true"))))
+      (go-test--go-test args env)))
 
   (defun okta-integration-test-current-file ()
     (interactive)
@@ -24,19 +26,17 @@
   (defun okta-integration-test-current-test (arg &optional last)
     "Launch go test on the current test. Largely copy/paste from go-test-current-test."
     (interactive "P")
-    (if arg
-        (call-interactively #'go-test-current-test last)
-      (progn
-        (unless (string-equal (symbol-name last) "last")
-          (setq go-test--current-test-cache (go-test--get-current-test-info)))
-        (when go-test--current-test-cache
-          (cl-destructuring-bind (test-suite test-name) go-test--current-test-cache
-            (let ((test-flag (if (> (length test-suite) 0) "-m " "-run "))
-                  (additional-arguments (if go-test-additional-arguments-function
-                                            (funcall go-test-additional-arguments-function
-                                                     test-suite test-name) "")))
-              (when test-name
-                (-okta-integration-test (s-concat test-flag test-name "\\$ . -v " additional-arguments)))))))))
+    (progn
+      (unless (string-equal (symbol-name last) "last")
+        (setq go-test--current-test-cache (go-test--get-current-test-info)))
+      (when go-test--current-test-cache
+        (cl-destructuring-bind (test-suite test-name) go-test--current-test-cache
+          (let ((test-flag (if (> (length test-suite) 0) "-m " "-run "))
+                (additional-arguments (if go-test-additional-arguments-function
+                                          (funcall go-test-additional-arguments-function
+                                                   test-suite test-name) "")))
+            (when test-name
+              (-okta-integration-test (s-concat test-flag test-name "\\$ . -v " additional-arguments) arg)))))))
 
   (with-eval-after-load 'go-mode
     (define-key go-mode-map (kbd "C-c t") #'okta-integration-test-current-test)
@@ -108,4 +108,62 @@
           (goto-char (point-min))
           (delete-region (point) (search-forward "\"stack\":\""))
           (delete-region (search-forward-regexp "[^\\]\"") (point-max))
-          (unescape-nt))))))
+          (unescape-nt)))))
+
+  (require 'auth-source)
+  (require 'ghub)
+  (defun auth-source-backend-git-credential-helper (entry)
+    (when (and (stringp entry) (string-match "^git-credential-helper" entry))
+      (auth-source-backend
+       :source  "git-credential-helper"
+       :type 'git-credential
+       :search-function #'auth-source-git-credential-helper-search)))
+
+
+  (defun auth-source-git-credential-helper--append (result key &optional filter)
+    (let* ((data (buffer-substring-no-properties (match-end 0) (line-end-position)))
+           (data (if (functionp filter) (funcall filter data (plist-get result key)) data)))
+      (plist-put result key data)))
+
+  (defun auth-source-git-credential-helper--process-output ()
+    (let ((ret '()))
+      (goto-char (point-min))
+      (while (not (eobp))
+        (cond
+         ((looking-at "^password=")
+          (setq ret (auth-source-git-credential-helper--append ret :secret (lambda (data &rest _)
+                                                                             (let ((v (auth-source--obfuscate data)))
+                                                                               (lambda () (auth-source--deobfuscate v)))))))
+         ((looking-at "^username=")
+          (setq ret (auth-source-git-credential-helper--append ret :user)))
+         ((looking-at "^host=")
+          (setq ret (auth-source-git-credential-helper--append ret :host (lambda (data &optional existing) (concat existing data)))))
+         ((looking-at "^protocol=")
+          (setq ret (auth-source-git-credential-helper--append ret :host (lambda (data &optional existing) (concat data "://" existing))))))
+        (forward-line))
+      ret))
+
+  (cl-defun auth-source-git-credential-helper-search (&rest spec
+                                                            &key create delete type
+                                                            &allow-other-keys)
+    (cl-assert (not create) nil "git-credential-helper doesn't support create")
+    (cl-assert (not delete) nil "git-credential-helper doesn't support delete")
+
+    (when (string-equal (plist-get spec :host) "api.github.com")
+      (let ((user (car (string-split (plist-get spec :user) "\\^")))
+            (helper (ignore-errors (car (process-lines "git" "config" "credential.helper")))))
+        (when (and user helper)
+          (let ((helper-and-args (append (string-split helper " ") (list "get")))
+                (out-buf (generate-new-buffer "output")))
+            (unwind-protect
+                (progn
+                  (apply #'call-process-region (format "username=%s\nprotocol=https\nhost=github.com\n\n" user) nil (car helper-and-args) nil out-buf nil (cdr helper-and-args))
+                  (with-current-buffer out-buf
+                    (let ((processed (auth-source-git-credential-helper--process-output)))
+                      (and (plist-get processed :secret) (list (plist-put processed :type type))))))
+              (kill-buffer out-buf)))))))
+
+
+
+  (add-hook 'auth-source-backend-parser-functions #'auth-source-backend-git-credential-helper)
+  (add-to-list 'auth-sources "git-credential-helper"))
